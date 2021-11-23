df94c4b42a240d4956203a98bd694dd893e3a576
Juan D. Martín <42278423+juamarCas@users.noreply.github.com>
2021-11-23 14:44:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,204 +1,73 @@
-/**
- *  LibEV.cpp
- * 
- *  Test program to check AMQP functionality based on LibEV
- * 
- *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
- *  @copyright 2015 - 2018 Copernica BV
- */
 
-/**
- *  Dependencies
- */
 #include <ev.h>
 #include <amqpcpp.h>
 #include <amqpcpp/libev.h>
-#include <openssl/ssl.h>
-#include <openssl/opensslv.h>
 
-/**
- *  Custom handler
- */
-class MyHandler : public AMQP::LibEvHandler
-{
-private:
-    /**
-     *  Method that is called when a connection error occurs
-     *  @param  connection
-     *  @param  message
-     */
+
+class MyHandler: public AMQP::LibEvHandler{
+
+    virtual void onAttached(AMQP::TcpConnection *connection) override
+    {
+
+     }
+    virtual void onConnected(AMQP::TcpConnection *connection) override
+    {
+
+    }
+
+    virtual bool onSecured(AMQP::TcpConnection *connection, const SSL *ssl) override
+    {
+        return true;
+    }
+
+    virtual void onReady(AMQP::TcpConnection *connection) override
+    {
+        std::cout<<"ready"<<std::endl;
+    }
+
     virtual void onError(AMQP::TcpConnection *connection, const char *message) override
     {
-        std::cout << "error: " << message << std::endl;
+
     }
 
-    /**
-     *  Method that is called when the TCP connection ends up in a connected state
-     *  @param  connection  The TCP connection
-     */
-    virtual void onConnected(AMQP::TcpConnection *connection) override 
+    virtual void onClosed(AMQP::TcpConnection *connection) override
     {
-        std::cout << "connected" << std::endl;
+
     }
 
-    /**
-     *  Method that is called when the TCP connection ends up in a ready
-     *  @param  connection  The TCP connection
-     */
-    virtual void onReady(AMQP::TcpConnection *connection) override 
+    virtual void onLost(AMQP::TcpConnection *connection) override
     {
-        std::cout << "ready" << std::endl;
+
     }
 
-    /**
-     *  Method that is called when the TCP connection is closed
-     *  @param  connection  The TCP connection
-     */
-    virtual void onClosed(AMQP::TcpConnection *connection) override 
+    virtual void onDetached(AMQP::TcpConnection *connection) override
     {
-        std::cout << "closed" << std::endl;
-    }
 
-    /**
-     *  Method that is called when the TCP connection is detached
-     *  @param  connection  The TCP connection
-     */
-    virtual void onDetached(AMQP::TcpConnection *connection) override 
-    {
-        std::cout << "detached" << std::endl;
-    }
-    
-    
-public:
-    /**
-     *  Constructor
-     *  @param  ev_loop
-     */
-    MyHandler(struct ev_loop *loop) : AMQP::LibEvHandler(loop) {}
-
-    /**
-     *  Destructor
-     */
-    virtual ~MyHandler() = default;
-};
-
-/**
- *  Class that runs a timer
- */
-class MyTimer
-{
-private:
-    /**
-     *  The actual watcher structure
-     *  @var struct ev_io
-     */
-    struct ev_timer _timer;
-
-    /**
-     *  Pointer towards the AMQP channel
-     *  @var AMQP::TcpChannel
-     */
-    AMQP::TcpChannel *_channel;
-
-    /**
-     *  Name of the queue
-     *  @var std::string
-     */
-    std::string _queue;
-
-
-    /**
-     *  Callback method that is called by libev when the timer expires
-     *  @param  loop        The loop in which the event was triggered
-     *  @param  timer       Internal timer object
-     *  @param  revents     The events that triggered this call
-     */
-    static void callback(struct ev_loop *loop, struct ev_timer *timer, int revents)
-    {
-        // retrieve the this pointer
-        MyTimer *self = static_cast<MyTimer*>(timer->data);
-
-        // publish a message
-        self->_channel->publish("", self->_queue, "Hello");
     }
 
 public:
-    /**
-     *  Constructor
-     *  @param  loop
-     *  @param  channel
-     *  @param  queue
-     */
-    MyTimer(struct ev_loop *loop, AMQP::TcpChannel *channel, std::string queue) : 
-        _channel(channel), _queue(std::move(queue))
-    {
-        // initialize the libev structure
-        ev_timer_init(&_timer, callback, 0.005, 1.005);
-
-        // this object is the data
-        _timer.data = this;
-
-        // and start it
-        ev_timer_start(loop, &_timer);
-    }
-    
-    /**
-     *  Destructor
-     */
-    virtual ~MyTimer()
-    {
-        // @todo to be implemented
-    }
+        MyHandler(struct ev_loop *loop): AMQP::LibEvHandler(loop){}
 };
 
 
-/**
- *  Main program
- *  @return int
- */
+
 int main()
 {
-    // access to the event loop
-    auto *loop = EV_DEFAULT;
-    
-    // handler for libev
-    MyHandler handler(loop);
+   auto *loop = EV_DEFAULT;
+   MyHandler myHandler(loop);
+   AMQP::Address address("amqp://guest:guest@localhost/");
+   AMQP::TcpConnection connection(&myHandler, address);
 
-    // make a connection
-    AMQP::Address address("amqp://guest:guest@localhost/");
-//    AMQP::Address address("amqps://guest:guest@localhost/");
-    AMQP::TcpConnection connection(&handler, address);
-    
-    // we need a channel too
-    AMQP::TcpChannel channel(&connection);
+   AMQP::TcpChannel channel(&connection);
+   channel.declareQueue("myqueue").onSuccess([&channel](const std::string &name, uint32_t messageCount, uint32_t consumercount){
+            std::cout<<"declared queue"<<std::endl;
+            channel.publish("","myqueue","hello world");
+            while(1){}
+   });
 
 
-    // create a temporary queue
-    channel.declareQueue("myqueue").onSuccess([&connection, &channel, loop](const std::string &name, uint32_t messagecount, uint32_t consumercount) {
-        
-        // report the name of the temporary queue
-        std::cout << "declared queue " << name << std::endl;
-        
-        // close the channel
-        //channel.close().onSuccess([&connection, &channel]() {
-        //    
-        //    // report that channel was closed
-        //    std::cout << "channel closed" << std::endl;
-        //    
-        //    // close the connection
-        //    connection.close();
-        //});
-      
-        // construct a timer that is going to publish stuff
-        auto *timer = new MyTimer(loop, &channel, name);
-        
-        //connection.close();
-    });
-    
-     
-    // run the loop
-    ev_run(loop, 0);
-  
-    // done
-    return 0;
+   ev_run(loop);
+
+   return 0;
+
 }
